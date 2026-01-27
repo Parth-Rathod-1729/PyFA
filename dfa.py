cb0f6cb6f7f0877e3a8e2abf3d5d7209ad2a66ad
Parth Rathod <parthrathod1729@gmail.com>
2026-01-27 09:55:40 +0530

    Update dfa.py
    - dfa.py now uses TransitionSymbol from utils

--- dfa.py
+++ dfa.py
@@ -1,49 +1,10 @@
 from __future__ import annotations
 from typing import Any
 from PyFA_Exception import StateError, TransitionSymbolError, MiscError
+from utils import TransitionSymbol
 
 #TODO: Implement NFA (without NULL transition)
 #TODO: Implement NFA (with NULL transition)
-
-class TransitionSymbol:
-    def __init__(self, symbolValue):
-        self.symbolValue = symbolValue
-
-    def __repr__(self):
-        return f"TransitionSymbol< {self.symbolValue.__str__()} >"
-
-    def __str__(self):
-        return self.symbolValue.__repr__()
-        
-    @staticmethod
-    def _flattenDict(flatable: dict[Any, Any]) -> tuple[tuple[Any]]:
-        flattened = list()
-        for key, value in sorted(flatable.items(), key=lambda kvPair: str(kvPair[0])):
-            if isinstance(value, dict):
-                flattened.append((key, TransitionSymbol._flattenDict(value)))
-            else:
-                flattened.append((key, value))
-        return tuple(flattened)
-
-    def __hash__(self) -> int:
-        try:
-            return self.symbolValue.__hash__()
-        except:
-            if isinstance(self.symbolValue, (list, set)):
-                return hash(tuple(self.symbolValue))
-            if isinstance(self.symbolValue, dict):
-                return hash(TransitionSymbol._flattenDict(self.symbolValue))
-            if type(self.symbolValue).__module__ != "builtins":
-                return hash(TransitionSymbol._flattenDict(self.symbolValue.__dict__))
-            print(f"TransitionSymbolWarning: The TransitionSymbol({self.symbolValue}) could not be hashed. Unexpected Errors may occur")
-            return id(self)
-
-    def __eq__(self, other: TransitionSymbol | Any) -> bool:
-        if isinstance(other, TransitionSymbol):
-            return self.symbolValue == other.symbolValue
-        else:
-            return self.symbolValue == other
-
 
 class State:
     def __init__(self, name: str, isFinal: bool = False, isInitial: bool = False,
